0d5d36fda32c9e8cbaeffd6b34622edaa05ffd76
ralcaraz <ralcarazerhs@gmail.com>
2022-12-19 23:02:03 -0800

    added logging to proxy stream

--- Pacman.NET/Middleware/PacmanPackageBody.cs
+++ Pacman.NET/Middleware/PacmanPackageBody.cs
@@ -5,77 +5,86 @@ namespace Pacman.NET.Middleware;
 
 public class PacmanPackageBody : Stream, IHttpResponseBodyFeature
 {
-    private readonly IHttpResponseBodyFeature _innerBodyFeature;
+    private readonly IHttpResponseBodyFeature _originalBodyFeature;
     private readonly FileStream _fileStream;
+    private readonly ILogger<PacmanPackageBody> _logger;
 
-    public PacmanPackageBody(IHttpResponseBodyFeature innerBodyFeature, FileStream fileStream)
+    public PacmanPackageBody(IHttpResponseBodyFeature originalBodyFeature, 
+        FileStream fileStream, 
+        ILogger<PacmanPackageBody> logger)
     {
-        _innerBodyFeature = innerBodyFeature;
+        _originalBodyFeature = originalBodyFeature;
         _fileStream = fileStream;
+        _logger = logger;
     }
 
 
     public void DisableBuffering()
     {
-        _innerBodyFeature.DisableBuffering();
+        _originalBodyFeature.DisableBuffering();
     }
 
 
     public async Task StartAsync(CancellationToken cancellationToken = new())
     {
+        _logger.LogInformation("Starting download for {FileName}", _fileStream.Name);
         await _fileStream.FlushAsync(cancellationToken);
-        await _innerBodyFeature.StartAsync(cancellationToken);
+        await _originalBodyFeature.StartAsync(cancellationToken);
     }
 
 
     public async Task SendFileAsync(string path, long offset, long? count, CancellationToken cancellationToken = new())
     {
+        _logger.LogInformation("Sending {FileName}", _fileStream.Name);
         await _fileStream.FlushAsync(cancellationToken);
-        await _innerBodyFeature.SendFileAsync(path, offset, count, cancellationToken);
+        await _originalBodyFeature.SendFileAsync(path, offset, count, cancellationToken);
     }
 
 
     public async Task CompleteAsync()
     {
-        await _innerBodyFeature.Stream.FlushAsync();
+        _logger.LogInformation("Completing {FileName}", _fileStream.Name);
+        await _originalBodyFeature.Stream.FlushAsync();
         await _fileStream.FlushAsync();
     }
 
     public Stream Stream => this;
-    public PipeWriter Writer => PipeWriter.Create(_innerBodyFeature.Stream, new StreamPipeWriterOptions(leaveOpen: true));
+    public PipeWriter Writer => PipeWriter.Create(_originalBodyFeature.Stream, new StreamPipeWriterOptions(leaveOpen: true));
     
     public override void Flush()
     {
+        _logger.LogInformation("Flushing {FileName}", _fileStream.Name);
         _fileStream.Flush();
-        _innerBodyFeature.Stream.Flush();
+        _originalBodyFeature.Stream.Flush();
     }
 
     public override async Task FlushAsync(CancellationToken cancellationToken)
     {
+        _logger.LogInformation("FlushAsync {FileName}", _fileStream.Name);
         await _fileStream.FlushAsync(cancellationToken);
-        await _innerBodyFeature.Stream.FlushAsync(cancellationToken);
+        await _originalBodyFeature.Stream.FlushAsync(cancellationToken);
     }
 
     public override int Read(byte[] buffer, int offset, int count)
     {
         _ = _fileStream.Read(buffer, offset, count);
-        return _innerBodyFeature.Stream.Read(buffer, offset, count);
+        return _originalBodyFeature.Stream.Read(buffer, offset, count);
     }
 
     public override long Seek(long offset, SeekOrigin origin)
     {
-        return _innerBodyFeature.Stream.Seek(offset, origin);
+        throw new NotSupportedException();
     }
 
     public override void SetLength(long value)
     {
-        _innerBodyFeature.Stream.SetLength(value);
+        _originalBodyFeature.Stream.SetLength(value);
         _fileStream.SetLength(value);
     }
 
     public override void Write(byte[] buffer, int offset, int count)
     {
-        _innerBodyFeature.Stream.Write(buffer, offset, count);
+        _originalBodyFeature.Stream.Write(buffer, offset, count);
         _fileStream.Write(buffer, offset, count);
     }
 
@@ -83,16 +92,16 @@ public class PacmanPackageBody : Stream, IHttpResponseBodyFeature
     {
         var memoryBuffer = buffer.AsMemory(offset, count);
         await _fileStream.WriteAsync(memoryBuffer, ctx);
-        await _innerBodyFeature.Stream.WriteAsync(memoryBuffer, ctx);
+        await _originalBodyFeature.Stream.WriteAsync(memoryBuffer, ctx);
     }
 
-    public override bool CanRead => _innerBodyFeature.Stream.CanRead;
-    public override bool CanSeek => _innerBodyFeature.Stream.CanSeek;
-    public override bool CanWrite => _innerBodyFeature.Stream.CanWrite;
-    public override long Length => _innerBodyFeature.Stream.Length;
+    public override bool CanRead => _originalBodyFeature.Stream.CanRead;
+    public override bool CanSeek => _originalBodyFeature.Stream.CanSeek;
+    public override bool CanWrite => _originalBodyFeature.Stream.CanWrite;
+    public override long Length => _originalBodyFeature.Stream.Length;
     public override long Position
     {
-        get => _innerBodyFeature.Stream.Position;
-        set => _innerBodyFeature.Stream.Position = value;
+        get => _originalBodyFeature.Stream.Position;
+        set => _originalBodyFeature.Stream.Position = value;
     }
 }
