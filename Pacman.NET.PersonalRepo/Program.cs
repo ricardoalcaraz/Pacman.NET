701f0e2ebe4371872add827b9991138d9371e43a
ralcaraz <ralcarazerhs@gmail.com>
2023-02-11 19:58:34 -0800

    added new webapi

---
+++ Pacman.NET.PersonalRepo/Program.cs
@@ -0,0 +1,34 @@
+using Pacman.NET.AbsoluteFileProvider;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDirectoryBrowser();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.UseFileServer(new FileServerOptions
+{
+    RequestPath = "/archlinux/ricardoalcaraz.dev",
+    FileProvider = new AbsoluteProvider("/data/archlinux/mirror"),
+    RedirectToAppendTrailingSlash = true,
+    EnableDirectoryBrowsing = true,
+    EnableDefaultFiles = false
+});
+app.Run();
--- Pacman.NET/Services/MirrorListParseService.cs
+++ Pacman.NET/Services/MirrorListParseService.cs
@@ -6,24 +6,21 @@ public class MirrorListParseService : IMirrorService
 {
     private readonly PacmanConfigParser _configParser;
     private readonly ILogger<MirrorListParseService> _logger;
-    private readonly IOptions<PacmanOptions> _options;
     private readonly MirrorClient _mirrorClient;
 
     public MirrorListParseService(PacmanConfigParser configParser, 
         ILogger<MirrorListParseService> logger, 
-        IOptions<PacmanOptions> options, 
         MirrorClient mirrorClient)
     {
         _configParser = configParser;
         _logger = logger;
-        _options = options;
         _mirrorClient = mirrorClient;
     }
 
 
     public async IAsyncEnumerable<string> MirrorUrlStream([EnumeratorCancellation] CancellationToken ctx)
     {
-        var mirrorUri = _options.Value.MirrorUrl;
+        var mirrorUri = "";
         _logger.LogInformation("Retrieving mirrors from file at {Dir}", mirrorUri);
 
         await foreach (var mirror in _configParser.ParseMirrorlist(mirrorUri, ctx))
